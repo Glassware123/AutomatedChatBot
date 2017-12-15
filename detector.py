6f1a84c67e360c06a75c0e8ccab8165af6fb8309
anil <anilbas1995@gmail.com>
2017-12-15 12:33:45 +0300

    detector stable v1.0.0

--- detector.py
+++ detector.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
+
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 from selenium.webdriver.common.keys import Keys
 from datetime import datetime
 import time
+import logging
 from selenium.common.exceptions import NoSuchElementException
-
-global elem1
 
 def check_exists_by_xpath():
     try:
@@ -14,26 +17,30 @@ def check_exists_by_xpath():
         return False
     return True
 
+def addLog():
+    logging.basicConfig(filename='lastseen.log',level=logging.INFO)
+    logging.info("BenSirket:" + datetime.strftime(now, "%c")+"\n" )
+    print "log"
+
+def checkStatusBar():
+    elem1 = web.find_element_by_xpath('//span[contains(@class,"emojitext O90ur")]')
+    return elem1
 
 web = webdriver.Firefox()
 web.get('http://web.whatsapp.com')
 time.sleep(10)
 
-try:
-    elem = web.find_element_by_xpath('//span[contains(text(),"XYZ")]')
-    elem.click()
-    while True:
-        time.sleep(10)
-        print check_exists_by_xpath()
-        if(check_exists_by_xpath()):
-            elem1 = web.find_elements_by_xpath('//span[contains(@class,"chat-subtitle-text")]')
+elem = web.find_element_by_xpath('//span[contains(text(),"BenSirket")]')
+elem.click()
+while True:
+    time.sleep(10)
+    print check_exists_by_xpath()
+    if(check_exists_by_xpath()):
+        try:
+            elem1 = checkStatusBar()
             now = datetime.now()
             print elem1
-            print elem1[0].text == "çevrimiçi"
-            if (elem1[0].text.decode("utf-8") == "çevrimiçi"):
-                print "log"
-                txt = open("log.txt", "a")
-                txt.write("XYZ" + datetime.strftime(now,"%c"))
+            addLog()
+        except:
+            time.sleep(10)
 
-except Exception as e:
-    print e.message
